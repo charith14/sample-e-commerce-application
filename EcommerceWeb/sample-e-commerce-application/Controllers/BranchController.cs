bb5ce9cbacaebbe1dea0d00fb8599c50d3c43ad4
charith <charithvishan100@gmail.com>
2020-10-17 04:43:07 +0530

    Add BranchController

--- EcommerceWeb/sample-e-commerce-application/Controllers/BranchController.cs
+++ EcommerceWeb/sample-e-commerce-application/Controllers/BranchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using sample_e_commerce_application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,68 @@ namespace sample_e_commerce_application.Controllers
 {
     public class BranchController : Controller
     {
-        // GET: Branch
+        private EcomContext ecomContext = new EcomContext();
+        // GET: Owner
         public ActionResult Index()
         {
+            List<Branch> AllBranch = ecomContext.Branches.ToList();
+            return View(AllBranch);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.BranchDetails = ecomContext.Branches;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Branch branch)
+        {
+
+            ecomContext.Branches.Add(branch);
+            ecomContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(String id)
+        {
+            Branch branch = ecomContext.Branches.SingleOrDefault(x => x.BranchNo == id);
+            return View(branch);
+        }
+
+        public ActionResult Edit(String id)
+        {
+            Branch branch = ecomContext.Branches.SingleOrDefault(x => x.BranchNo == id);
+            //ViewBag.DeptDetails = new SelectList(ecomContext.Owners, "DeptId", "Name");
+            return View(branch);
+        }
+        [HttpPost]
+        public ActionResult Edit(String id, Branch updatedBrach)
+        {
+            Branch branch = ecomContext.Branches.SingleOrDefault(x => x.BranchNo == id);
+            branch.BranchNo = updatedBrach.BranchNo;
+            branch.Street = updatedBrach.Street;
+            branch.City = updatedBrach.City;
+            branch.PostCode = updatedBrach.PostCode;
+           
+
+            ecomContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(String id)
+        {
+            Branch branch = ecomContext.Branches.SingleOrDefault(x => x.BranchNo == id);
+            return View(branch);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteEmployee(String id)
+        {
+            Branch branch = ecomContext.Branches.SingleOrDefault(x => x.BranchNo == id);
+            ecomContext.Branches.Remove(branch);
+            ecomContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
